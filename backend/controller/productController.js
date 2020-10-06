54b5fd0ce00a9ae8a7bc00020ebf143a94901f32
wattanaitsp <wattanai@nsquared.asia>
2020-10-07 00:06:32 +0700

    add: add productController

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,7 +1,36 @@
 import Product from '../model/mongooseModel';
 
-async function getProduct(itemId){
 
-    let product = Product.findOne({})
+async function getAllProduct(){
+
+    let products = await Product.find({}).exec();
+
+    return products
 
 }
+
+async function getProduct(productId){
+
+    if (!itemId) return false
+    let product = await Product.findById(productId).exec();
+    if (product){
+        return product
+    }
+    else{
+        return null
+
+    }
+
+}
+
+async function addProduct(product){
+    
+    let newProduct = await Product.create(
+        product
+    ).exec();
+    
+    return newProduct
+
+}
+
+export { getAllProduct, getProduct, addProduct }
