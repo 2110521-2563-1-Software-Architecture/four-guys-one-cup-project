37ebedb27d5b336bf7c614700f7a79739647a9ab
Knot <knot131-wr@hotmail.com>
2020-10-06 21:40:30 +0700

    change db name

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/4guys1cup', {useNewUrlParser: true});
 
 const productSchema = new Schema({
   name:  String,
