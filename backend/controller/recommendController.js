90d403381ebaae6452fca576e79868dfc4eebfc9
timeac131 <tyongyai@hotmail.com>
2020-10-27 21:43:59 +0700

    for es6

--- backend/controller/recommendController.js
+++ backend/controller/recommendController.js
@@ -1,7 +1,7 @@
 import User from '../model/mongooseModel'
 import Product from '../model/mongooseModel'
 
-const RecommendInterface = class{
+const RecommendProductInterface = class{
     constructor(){
         if(!this.sort){
             throw new Error('Recommend Class must has sort medthod')
@@ -10,4 +10,29 @@ const RecommendInterface = class{
 
 }
 
-const Recommend 
+
+const ContentBasedFiltering = class extends RecommendProductInterface{
+    constructor(){
+        
+    }
+    distanceFuntion(name){
+        let distanceScore = 0
+        if(name == 'dot-product'){
+            // TODO //
+        }
+        else if(name == 'consine'){
+
+        }
+        return distanceScore /*return score*/
+    }
+    createScoreVector(userVector, productVector, scoreFunction){
+        
+        // TODO //
+    }
+    sort(userVector, productVector, scoreFunction){
+        let scoreVector = createScoreVector(userVector, productVector, scoreFunction)
+        // TODO //
+        /* sort product accordding to scoreVector */
+        
+    }
+}
