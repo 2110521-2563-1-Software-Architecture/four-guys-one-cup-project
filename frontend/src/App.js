803e12ce6c71bc44681a1acf0a6a71e101a8b95d
wattanaitsp <wattanai@nsquared.asia>
2020-10-28 17:30:03 +0700

    add: login panel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{ useState, useEffect }from 'react';
+import axios from 'axios';
 import './App.css';
+import LoginForm from './components/Login/LoginForm'
+
+const apiUrl = 'http://localhost:9000';
+
+axios.interceptors.request.use(
+  config => {
+    const { origin } = new URL(config.url);
+    const allowedOrigins = [apiUrl];
+    const token = localStorage.getItem('token');
+    if (allowedOrigins.includes(origin)) {
+      config.headers.authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 function App() {
+
+  const storedJwt = localStorage.getItem('token');
+  const [jwt, setJwt] = useState(storedJwt || null);
+
+  useEffect(() => {
+    const getJwt = async () => {
+      // const { data } = await axios.get(`${apiUrl}/jwt`);
+      let data = {"token": "dummyToken"};
+      localStorage.setItem('token', data.token);
+      setJwt(data.token);
+    };
+
+    const clearJwt = async () => {
+      // const { data } = await axios.get(`${apiUrl}/jwt/clear`);
+      let data = {"token": null};
+      localStorage.setItem('token', null);
+      setJwt(null);
+    }
+    
+    getJwt();
+
+    return function cleanup() {
+      clearJwt();
+    };
+  });
+
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* nav-bar */}
+        <h1>HEADER</h1>
       </header>
+      <div className='App-body'>
+        <LoginForm></LoginForm>
+      </div>
+      <footer className='App-footer'>
+        {/* footer */}
+        <h1>FOOTER</h1>
+      </footer>
     </div>
   );
 }
---
+++ frontend/src/components/Login/LoginForm.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const onLogin = async () => {
+
+        const data = {
+            "email": email,
+            "password": password
+        }
+
+        const response = await fetch("/api/users/login", {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        })
+
+        const user = response.json()
+
+        return user
+
+    }
+
+    return (
+        <div className="card">
+            <div class="card-body">
+                <h4 class="card-title mb-4">Sign-in</h4>
+                <form>
+                    <div className='form-group'>
+                        <label>Email</label>
+                        <input className='form-control' placeholder='ex@sample.com' type='email' onChange={(event)=>setEmail(event.target.value)}/>
+                    </div>
+                    <div className='form-group'>
+                        <label>Password</label>
+                        <input className='form-control' placeholder='*********' type='password' onChange={(event)=>setPassword(event.target.value)}/>
+                    </div>
+                    <div className='form-group'>
+                        <button type="submit" className="btn btn-primary btn-block" onSubmit={onLogin}> Login  </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login
